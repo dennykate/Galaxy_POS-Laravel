7d66b9d31e4bb832e7e291d094b6a31f54fe99c2
dennykate <dennykate22@gmail.com>
2024-09-07 23:34:46 +0630

    fix: order analysis bug fix

--- app/Http/Controllers/Voucher/MenuController.php
+++ app/Http/Controllers/Voucher/MenuController.php
@@ -16,11 +16,11 @@ class MenuController extends Controller
         $startDate = Carbon::createFromFormat('d-m-Y', $request->input('start_date'))->startOfDay();
         $endDate = Carbon::createFromFormat('d-m-Y', $request->input('end_date'))->endOfDay();
 
+
         // Get the list of products grouped by summing their quantity
         $products = VoucherRecord::select('product_id')
             ->selectRaw('SUM(quantity) as total_quantity')
             ->whereHas('voucher', function ($query) use ($startDate, $endDate) {
-                $query->whereNot('status', 'ရှင်းပြီး');
                 $query->whereBetween('order_date', [$startDate, $endDate]);
             })
 
@@ -28,14 +28,13 @@ class MenuController extends Controller
             ->with('product:id,name,image,actual_price,primary_price')
             ->get()
             ->map(function ($data) {
-                $data['product']['quantity'] = intval($data['total_quantity']);
+                $data['product']['quantity'] = $data['total_quantity'];
 
                 return $data['product'];
             });
 
         // Get the list grouped by customer_city and count it
         $cities = Voucher::select('customer_city as city')
-            ->whereNot('status', 'ရှင်းပြီး')
             ->selectRaw('COUNT(*) as total')
             ->whereBetween('order_date', [$startDate, $endDate])
             ->groupBy('customer_city')
